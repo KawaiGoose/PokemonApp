c3bd8973222b89a8a2b8d48d2c67044337a49154
YangSong <songyanghhhhhh@gmail.com>
2023-12-01 02:36:41 -0600

    提交信息

--- src/theme.tsx
+++ src/theme.tsx
@@ -22,7 +22,6 @@ const theme = createMuiTheme({
       primary: "#fff",
       secondary: "rgba(255, 255, 255, 0.7)",
       disabled: "rgba(255, 255, 255, 0.38)",
-      hint: "rgba(255, 255, 255, 0.38)",
     },
   },
   typography: {
