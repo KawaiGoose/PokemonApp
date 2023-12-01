f9131040bb20de59df2c8228193b2ab6d395f71d
YangSong <songyanghhhhhh@gmail.com>
2023-11-30 19:29:41 -0600

    提交信息

--- src/app/pokemonsearch/page.tsx
+++ src/app/pokemonsearch/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react';
-import { useRouter } from 'next/navigation'; // 注意：这里是 'next/router'，不是 'next/navigation'
+import { useRouter } from 'next/router'; // 注意：这里是 'next/router'，不是 'next/navigation'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
